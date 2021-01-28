989c945920266045c5d776e8f800ec6484e5cf61
Juraj Kirchheim <back2dos@gmail.com>
2021-01-28 17:54:50 +0100

    Minimal example of in-place updates messing things up.

--- tests/TodoMvc.hx
+++ tests/TodoMvc.hx
@@ -4,6 +4,7 @@ import coconut.vdom.*;
 import coconut.fake.Tags.*;
 import coconut.fake.*;
 #end
+import tink.state.*;
 import coconut.data.*;
 import tink.unit.*;
 import tink.testrunner.*;
@@ -11,41 +12,48 @@ using tink.CoreApi;
 
 @:asserts
 class TodoMvc {
+  var root:#if coconut.vdom js.html.Element #else Dummy #end;
+  var items:State<List<TodoItem>>;
 
   function new() {
 
   }
+  @:before public function init() {
+    items = new State(null);
+    root = createRoot();
+    return Promise.NOISE;
+  }
+
+  function createRoot() {
+    return
+      #if coconut.vdom
+        js.Browser.document.createElement('main')
+      #else
+        new Dummy('main')
+      #end
+    ;
+  }
+
+  function byClass(name:String)
+    return
+      #if coconut.vdom
+        [for (e in root.getElementsByClassName(name)) e];
+      #else
+        root.find(d -> d.get('className') == name);
+      #end
+
+  function update(value) {
+    items.set(value);
+    Renderer.updateAll();
+  }
+
+  function add(desc)
+    update(items.value.prepend(new TodoItem({ description: desc })));
+
   @:variant(false)
   @:variant(true)
-  public function testBasic(keyed:Bool) {
-    var items = new tink.state.State<List<TodoItem>>(null),
-        root =
-          #if coconut.vdom
-            js.Browser.document.createElement('main')
-          #else
-            new Dummy('root')
-          #end
-        ;
+  public function testInsertion(keyed:Bool) {
 
-    function byClass(name:String)
-      return
-        #if coconut.vdom
-          [for (e in root.getElementsByClassName(name)) e];
-        #else
-          root.find(d -> d.get('className') == name);
-        #end
-
-    function update(value) {
-      items.set(value);
-      Renderer.updateAll();
-      var footerCount = Std.parseInt(byClass("footer")[0].innerHTML);
-      if (footerCount != items.value.length)// don't wanna spam so much
-        asserts.assert(footerCount == items.value.length);
-    }
-
-    function add(desc) {
-      update(items.value.prepend(new TodoItem({ description: desc })));
-    }
     Renderer.mount(root, '<TodoListView list=${items} keyed=${keyed} />');
     function descriptions()
       return [for (d in byClass("todo-item-description")) d.innerHTML].join(',');
@@ -62,6 +70,21 @@ class TodoMvc {
 
     return asserts.done();
   }
+
+  @:include
+  public function testUpdates() {
+    var filter = new State(Complete);
+    add('a');
+    Renderer.mount(root, '<TodoListView filter=${filter} list=${items} />');
+    var before = root.innerHTML;
+    filter.set(All);
+    Renderer.updateAll();// <-- this update causes dom out of order
+    filter.set(Complete);
+    Renderer.updateAll();
+    asserts.assert(root.innerHTML == before);
+    return asserts.done();
+  }
+
   static function main() {
     Runner.run(TestBatch.make([
       new TodoMvc()
@@ -69,7 +92,7 @@ class TodoMvc {
   }
 }
 
-class TodoItem implements Model {
+private class TodoItem implements Model {
   @:editable var done:Bool = false;
   @:editable var description:String;
   public function toString() {
@@ -77,27 +100,58 @@ class TodoItem implements Model {
   }
 }
 
+enum abstract Filter(String) to String {
+  var All;
+  var Active;
+  var Complete;
+  public inline function includes(i:TodoItem)
+    return switch (cast this:Filter) {
+      case All: true;
+      case Active: !i.done;
+      case Complete: i.done;
+    }
+}
+
 class TodoListView extends View {
   @:attribute var list:List<TodoItem>;
-  @:attribute var keyed:Bool;
+  @:controlled var filter:Filter = All;
+  @:attribute var keyed:Bool = true;
   function render() '
     <div class="todo-list">
+      <Header list=${list} />
       <for ${item in list}>
-        <TodoItemView key=${if (keyed) item else null} item=${item} />
-      </for>
-      <div class="footer">
-        <if ${list.length > 0}>
-          ${switch list.count(i -> !i.done) {
-            case 1: '1 item left';
-            case v: '$v items left';
-          }}
+        <if ${filter.includes(item)}>
+          <TodoItemView key=${if (keyed) item else null} item=${item} />
         </if>
-      </div>
+      </for>
+      <Footer list=${list} filter=${filter} />
     </div>
   ';
 }
 
-class TodoItemView extends View {
+private class Header extends View {
+  @:attribute var list:List<TodoItem>;
+  function render() '
+    <div id="header" class=${if (list.length == 0) "idle" else "busy"} />
+  ';
+}
+
+private class Footer extends View {
+  @:attribute var list:List<TodoItem>;
+  @:controlled var filter:Filter = All;
+  function render() '
+    <div id="footer" class=${filter}>
+      <if ${list.length > 0}>
+        ${switch list.count(i -> !i.done) {
+          case 1: '1 item left';
+          case v: '$v items left';
+        }}
+      </if>
+    </div>
+  ';
+}
+
+private class TodoItemView extends View {
   @:attribute var item:TodoItem;
   function render() '
     <div class="todo-item">
