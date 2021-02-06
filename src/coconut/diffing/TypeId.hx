2d6fbf0d4a461ec0a223cd9593c78aec3ef03292
Juraj Kirchheim <back2dos@gmail.com>
2021-02-06 20:38:15 +0100

    Work around static initialization issues.

--- src/coconut/diffing/TypeId.hx
+++ src/coconut/diffing/TypeId.hx
@@ -1,7 +1,9 @@
 package coconut.diffing;
 
 abstract TypeId(Int) to Int {
-  static var idCounter = 0;
+  static var idCounter:Int;
+  static function __init__()
+    idCounter++;
   public inline function new()
     this = idCounter++;
 }
