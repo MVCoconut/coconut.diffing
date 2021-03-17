51c1fd095a205d80d5527248676fb5b32980816e
Juraj Kirchheim <back2dos@gmail.com>
2021-03-17 20:09:14 +0100

    Fix VNativeInst

--- src/coconut/diffing/VNode.hx
+++ src/coconut/diffing/VNode.hx
@@ -4,7 +4,7 @@ import coconut.diffing.internal.VNode as Internal;
 
 @:transitive
 abstract VNode<Native>(Internal<Native>) from Internal<Native> to Internal<Native> {
-  static public inline function embed<Native>(n:Native):VNode<Native>
+  static public inline function embed<Native:{}>(n:Native):VNode<Native>
     return new VNativeInst(n);
 
   static public inline function many<Native, RenderResult:VNode<Native>>(c:Children<RenderResult>):VNode<Native>
--- src/coconut/diffing/internal/VNativeInst.hx
+++ src/coconut/diffing/internal/VNativeInst.hx
@@ -1,14 +1,15 @@
 package coconut.diffing.internal;
 
-class VNativeInst<Native> implements VNode<Native> implements RNode<Native> {
+class VNativeInst<Native:{}> implements VNode<Native> implements RNode<Native> {
   static final TYPE = new TypeId();
   public final type:TypeId = TYPE;
-  public final key:Key = null;// perhaps this should simply be the instance itself
+  public final key:Key;// perhaps this should simply be the instance itself
   public final isSingular = true;
   final native:Native;
 
   public function new(native) {
     this.native = native;
+    this.key = native;
   }
 
   public function render(_, cursor:Cursor<Native>, later) {//TODO: consider detecting double mounting
@@ -22,12 +23,8 @@ class VNativeInst<Native> implements VNode<Native> implements RNode<Native> {
   public function justInsert(cursor, _)
     cursor.insert(native);
 
-  public function update(next:VNode<Native>, cursor:Cursor<Native>, _):Void {
-    var next = Cast.down(next, VNativeInst);
-    cursor.insert(next.native);
-    if (next.native != native)
-      cursor.delete(1);
-  }
+  public function update(next:VNode<Native>, cursor:Cursor<Native>, _):Void
+    inline justInsert(cursor, _);
 
   public function destroy(_):Int
     return 1;
